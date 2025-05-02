76e99cc0dbc32f9ff280e72b2eef087ef46a4d0a
Kasatskaya-DV <Kasatskaya-DV@ao-rr.ru>
2025-05-02 16:34:14 +0300

    Первый экран.Верстка

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.mifareread.playlistmaker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mifareread.playlistmaker"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
