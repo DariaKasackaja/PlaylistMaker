06e53e6561ffd7495e05dca1b5f0835fcfd24224
Kasatskaya-DV <Kasatskaya-DV@ao-rr.ru>
2025-05-03 22:31:40 +0300

    Реализованно нажатие на кнопки главного экрана с заглушками(Домашнее задание 2)

--- app/src/main/java/com/mifareread/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/mifareread/playlistmaker/MainActivity.kt
@@ -2,10 +2,39 @@ package com.mifareread.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonLibrary = findViewById<Button>(R.id.library)
+        val buttonSettings = findViewById<Button>(R.id.settings)
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                Toast.
+                makeText(this@MainActivity, "Нажата кнопка \"Поиск\"", Toast.LENGTH_SHORT).
+                show()
+            }
+        }
+
+        buttonSearch.setOnClickListener(buttonClickListener)
+
+        buttonLibrary.setOnClickListener{
+            Toast.
+            makeText(this@MainActivity, "Нажата кнопка \"Медиатека\"", Toast.LENGTH_SHORT).
+            show()
+        }
+
+        buttonSettings.setOnClickListener {
+            Toast.
+            makeText(this@MainActivity, "Нажата кнопка \"Настройки\"", Toast.LENGTH_SHORT).
+            show()
+        }
     }
 }
