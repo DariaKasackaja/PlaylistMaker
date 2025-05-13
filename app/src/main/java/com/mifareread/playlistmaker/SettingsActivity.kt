fd053e0f7b48cacefa600fe50e9a1ea8c11d25a0
Kasatskaya-DV <Kasatskaya-DV@ao-rr.ru>
2025-05-14 00:43:50 +0300

    Добавлены обработчики кнопок экрана "Настройки". Спринт 9. Домашнее задание 2.

--- app/src/main/java/com/mifareread/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/mifareread/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,12 @@
 package com.mifareread.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.net.URL
 
 class SettingsActivity: AppCompatActivity() {
 
@@ -12,5 +16,31 @@ class SettingsActivity: AppCompatActivity() {
 
         val buttonBack = findViewById<Button>(R.id.back_button)
         buttonBack.setOnClickListener { finish() }
+
+        val buttonShare=findViewById<TextView>(R.id.share_button)
+        buttonShare.setOnClickListener {
+            val intentShare = Intent(Intent.ACTION_SEND)
+            intentShare.setType("text/plain")
+            intentShare.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text))
+            startActivity(Intent.createChooser(intentShare, getString(R.string.share_question) ))
+
+        }
+
+        val buttonSupport = findViewById<TextView>(R.id.support_button)
+        buttonSupport.setOnClickListener {
+            val intentSupport = Intent(Intent.ACTION_SENDTO)
+            intentSupport.data = Uri.parse("mailto:")
+            intentSupport.putExtra(Intent.EXTRA_EMAIL, getString(R.string.mail))
+            intentSupport.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_name))
+            intentSupport.putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_text))
+            startActivity(intentSupport)
+        }
+
+        val buttonForward = findViewById<TextView>(R.id.forward_button)
+        buttonForward.setOnClickListener {
+            val intentForward = Intent(Intent.ACTION_VIEW,
+                Uri.parse(getString(R.string.forward_url)) )
+            startActivity(intentForward)
+        }
     }
 }
