905bfb19b72d3c703cd06f536c910bf04d410508
Kasatskaya-DV <Kasatskaya-DV@ao-rr.ru>
2025-05-16 02:09:35 +0300

    Смоделирован экран "Поиск", добавлены обработчики элементов. Спринт 9. Домашнее задание 3.

--- app/src/main/java/com/mifareread/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/mifareread/playlistmaker/SearchActivity.kt
@@ -1,12 +1,58 @@
 package com.mifareread.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class SearchActivity: AppCompatActivity() {
+class SearchActivity:AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activit_search)
+        setContentView(R.layout.activity_search)
+
+        val buttonClear = findViewById<ImageView>(R.id.clearIcon)
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val buttonBack = findViewById<Button>(R.id.search_button)
+
+
+
+        buttonClear.setOnClickListener{ view ->
+            searchEditText.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+
+        buttonBack.setOnClickListener {
+            finish()
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val test = p0.toString()
+
+                buttonClear.visibility= if (p0.isNullOrEmpty())
+                { View.GONE }
+                else{View.VISIBLE}
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        }
+        searchEditText.addTextChangedListener(searchTextWatcher)
     }
+
 }
+
--- app/src/main/java/com/mifareread/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/mifareread/playlistmaker/SettingsActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.net.URL
 
 class SettingsActivity: AppCompatActivity() {
 
