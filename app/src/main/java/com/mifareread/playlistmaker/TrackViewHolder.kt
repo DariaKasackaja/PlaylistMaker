252e3a937fab6de3d5943be415ac46dd218f2bd0
Kasatskaya-DV <Kasatskaya-DV@ao-rr.ru>
2025-05-30 06:09:22 +0300

    Спринт 10. Наработки

---
+++ app/src/main/java/com/mifareread/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,38 @@
+package com.mifareread.playlistmaker
+
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+
+    private val cover:ImageView = itemView.findViewById(R.id.cover)
+    private val trackTittle:TextView = itemView.findViewById(R.id.track_title)
+    private val trackArtist:TextView = itemView.findViewById(R.id.track_artist)
+    private val trackTime:TextView = itemView.findViewById(R.id.track_time)
+
+    fun bind( track : Track ){
+        Glide.with(itemView)
+            .load(track.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(dpToPx(2.0f, itemView)))
+                .into(cover)
+
+        trackTittle.text = track.trackName
+        trackTime.text = track.trackTime
+        trackArtist.text = track.artistName
+
+    }
+
+    private fun dpToPx(dp: Float, context: View): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics).toInt()
+    }
+}
