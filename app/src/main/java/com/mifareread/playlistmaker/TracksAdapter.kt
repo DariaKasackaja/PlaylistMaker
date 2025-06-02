c82478ec8d051c89d9b9482fc281befedb073a9a
Kasatskaya-DV <Kasatskaya-DV@ao-rr.ru>
2025-06-02 06:16:39 +0300

    Спринт 12. Добавлен экран с иторией выбора

--- app/src/main/java/com/mifareread/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/mifareread/playlistmaker/TracksAdapter.kt
@@ -3,14 +3,17 @@ package com.mifareread.playlistmaker
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class TracksAdapter():
+class TracksAdapter( private val trackItemClickListener: TrackClickListener ):
     RecyclerView.Adapter<TrackViewHolder>() {
 
         var tracks = mutableListOf<Track>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            TrackViewHolder {
         val view =LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent,false)
         return TrackViewHolder(view)
     }
@@ -18,6 +21,15 @@ class TracksAdapter():
     override fun getItemCount(): Int = tracks.size
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-        holder.bind(tracks.get(position))
+        holder.bind(tracks[position])
+        holder.itemView.setOnClickListener {
+
+            trackItemClickListener.
+            onTrackClick( tracks[ holder.adapterPosition ] )
+        }
+    }
+
+    fun interface TrackClickListener{
+        fun onTrackClick( track : Track )
     }
 }
