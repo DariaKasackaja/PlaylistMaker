3829e44ee021f5db26797a298a88695dfa328383
Kasatskaya-DV <Kasatskaya-DV@ao-rr.ru>
2025-06-02 07:13:40 +0300

    Revert "Revert "Revert "Revert "Спринт 12. Добавлен экран с иторией выбора""""
    This reverts commit 43f0cd75d1e407dbf156f95c567b9797a10bdd7c.

---
+++ app/src/main/java/com/mifareread/playlistmaker/TracksSelectedAdapter.kt
@@ -0,0 +1,23 @@
+package com.mifareread.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TracksSelectedAdapter(): RecyclerView.Adapter<TrackViewHolder>(){
+
+    var tracks = mutableListOf<Track>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.track_view, parent, false)
+
+        return TrackViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = tracks.size
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(tracks[position])
+    }
+}
