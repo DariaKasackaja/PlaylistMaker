798daeb37b9695082bb07fd8b7d8afe0fab17748
Kasatskaya-DV <Kasatskaya-DV@ao-rr.ru>
2025-06-02 06:58:27 +0300

    Revert "Спринт 12. Добавлен экран с иторией выбора"
    This reverts commit c82478ec8d051c89d9b9482fc281befedb073a9a.

--- app/src/main/java/com/mifareread/playlistmaker/App.kt
+++ app/src/main/java/com/mifareread/playlistmaker/App.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatDelegate
 
 const val PLAYLIST_PREFERENCES = "playlist_preferences"
 const val SWITCH_THEME_KEY = "key_for_switch_theme"
-const val SEARCH_HISTORY_KEY = "key_for_search_history"
 
 class App : Application() {
 
     var darkTheme = false
+    lateinit var sharedPreferences :SharedPreferences
 
     override fun onCreate() {
         super.onCreate()
 
-        val sharedPreferences =  getSharedPreferences(PLAYLIST_PREFERENCES, MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences(PLAYLIST_PREFERENCES, MODE_PRIVATE)
         darkTheme = sharedPreferences.getBoolean(SWITCH_THEME_KEY, false)
         switchTheme(darkTheme)
     }
@@ -27,7 +27,6 @@ class App : Application() {
             else{ AppCompatDelegate.MODE_NIGHT_NO }
         )
 
-        val sharedPreferences =  getSharedPreferences(PLAYLIST_PREFERENCES, MODE_PRIVATE)
         sharedPreferences.edit()
             .putBoolean(SWITCH_THEME_KEY, darkTheme)
             .apply()
--- app/src/main/java/com/mifareread/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/mifareread/playlistmaker/TracksAdapter.kt
@@ -3,17 +3,14 @@ package com.mifareread.playlistmaker
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class TracksAdapter( private val trackItemClickListener: TrackClickListener ):
+class TracksAdapter():
     RecyclerView.Adapter<TrackViewHolder>() {
 
         var tracks = mutableListOf<Track>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            TrackViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view =LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent,false)
         return TrackViewHolder(view)
     }
@@ -21,15 +18,6 @@ class TracksAdapter( private val trackItemClickListener: TrackClickListener ):
     override fun getItemCount(): Int = tracks.size
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-        holder.bind(tracks[position])
-        holder.itemView.setOnClickListener {
-
-            trackItemClickListener.
-            onTrackClick( tracks[ holder.adapterPosition ] )
-        }
-    }
-
-    fun interface TrackClickListener{
-        fun onTrackClick( track : Track )
+        holder.bind(tracks.get(position))
     }
 }
